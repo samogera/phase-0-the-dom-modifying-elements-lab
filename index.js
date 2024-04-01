c857f3f9f020221ec7113d49ea168fbfee1af4c8
samogera <samuelohillary@gmail.com>
2024-04-01 10:11:48 +0300

    dom manipulation

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ main.remove();
 const newHeader = document.createElement('h1');
 newHeader.id = 'victory';
 newHeader.textContent = 'Your Name is the champion';
-newHeader.textContent = newHeader.textContent.replace('Your Name', 'Samuel'); // Replace this with your name
+newHeader.textContent = newHeader.textContent.replace('Your Name', 'Samuel');
 
 document.body.appendChild(newHeader);
